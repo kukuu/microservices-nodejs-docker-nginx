d393a53e937ec869434c1e7675b665ca310b9ce7
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-05-23 17:52:56 +0100

    Initial commit Book micro-service.
    1. Server.js
    2. DB_URI
    3. Config

---
+++ books/src/server.js
@@ -0,0 +1,12 @@
+
+//Server.js file for book service
+//will be replicated in video service
+const  app = require("./src/app");
+const { DB_URI } =require("./src/config");
+const mongoose = require("mongoose");
+mongoose.connect(DB_URI);
+
+app.listen(3000, () => {
+	console.log("Running on port 8000");
+	console.log(".....................")
+});
