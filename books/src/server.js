1e82f3d93881553fc3213eb2aa60e744dd50581f
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-05-24 10:11:07 +0100

    Updated package.json
    Added start script.

--- books/src/server.js
+++ books/src/server.js
@@ -6,7 +6,7 @@ const { DB_URI } =require("./src/config");
 const mongoose = require("mongoose");
 mongoose.connect(DB_URI);
 
-app.listen(8000, () => {
+app.listen(3000, () => {
 	console.log("Running on port 8000");
 	console.log(".....................")
 });
