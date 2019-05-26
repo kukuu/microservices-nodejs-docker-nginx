221f28307d96d30a199a26c51f3f37b881eba900
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-05-26 22:51:44 +0100

    First commit app.js
    1. GET request
    2. POST request
    3. API versioning

---
+++ books/src/app.js
@@ -0,0 +1,32 @@
+
+const express = require("express");
+const Book = require("./models/books_model");
+const app = express(); //Instantiate express
+const bodyParser = require("body-parser");// request/updating DB
+
+app.use(bodyParser.json());
+
+
+//Requesting  API resource - route
+app.get("/", (req, res ) => {
+	res.json({ msg: "Books Domain"})
+});
+
+
+//GET :: Versioning API: "books"
+app.get("/api/v1/books", async () => {
+	//cache request
+	const books = await Book.find({})
+	res.json(books);
+})
+
+//POST - sending object to DB
+app.post("", async () => {
+	//cache instance post object
+	const book = new Book({ name: req.body.name });
+	const savedBook = await book.saved();
+	res.json(savedBook);
+});
+
+
+module.exports = app;
