faf98dcac632868c4ab1b0f15012c03c6f269fe1
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-06-04 17:09:05 +0100

    Fetch data for video and book micro-services
    Cache and use abstract function name

--- web/public/script.js
+++ web/public/script.js
@@ -10,15 +10,29 @@
 			}
 			return false;
 		}
+
+		//Create an INTERFACE for a  named instance of the Abstract function
+		//Re-use same function to make requests
+		const makePostNameFunc = uri => val => {
+			fetch(uri, {
+				method: "POST",
+				body: JSON.stringify({name: val }),
+				header: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				}
+			});
+		};
+
+		//fetch data for video and book micro-services and cache re-use function name
+		const postVideoName = makePostNameFunc("https://localhost:8080/api/v1/videos");
+		const postVideoName = makePostNameFunc("https://localhost:8080/api/v1/books");
+
+
+		//Build SEARCH procedure
+
+		//return results
 	})
 
-//Create an INTERFACE for a  named instance of the Abstract function
 
 
-//Dispatch 2 to fetch data 
-
-
-//Build SEARCH procedure
-
-
-//return results
