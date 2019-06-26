0dd65a4eeccd11ef042ec56d17f41b1f8c326abf
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-06-26 17:03:54 +0100

    Updated Server.js
    Port number

--- books/server.js
+++ books/server.js
@@ -3,11 +3,11 @@
 //One of 3 micro-services
 //will be replicated in video service
 const  app = require("./src/app");
-const { DB_URI } =require("./src/config");
+const { DB_URI } = require("./src/config");
 const mongoose = require("mongoose");
 mongoose.connect(DB_URI);
 
-app.listen(5000, () => {
+app.listen(3000, () => {
 	console.log("Running on port 8000");
 	console.log(".....................")
 });
