93aeba73701cb2d2077c060fff63e75a3ecae700
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-07-18 15:18:00 +0100

    Dynamic IP addresses assignment for containers.

--- books/src/config/index.js
+++ books/src/config/index.js
@@ -1,6 +1,7 @@
 let DB_URI = "mongodb://localhost:27017/microservices-nodejs-docker-nginx";
 
 //Will be re-configured with nginx with service name for proxy-ing
+//The containers IP addresses are dynamic, and can be deployed from any available pot that is available
 if(process.env.MONGO_DB_URI){
 	DB_URI = process.env.MONGO_DB_URI
 }
