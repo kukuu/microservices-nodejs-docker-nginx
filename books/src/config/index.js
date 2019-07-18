49ed832da266bfb4b5af032eb38e2cb38bd73d1b
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-07-18 15:20:38 +0100

    Proxying by service names - NGINX

--- books/src/config/index.js
+++ books/src/config/index.js
@@ -2,6 +2,7 @@ let DB_URI = "mongodb://localhost:27017/microservices-nodejs-docker-nginx";
 
 //Will be re-configured with nginx with service name for proxy-ing
 //The containers IP addresses are dynamic, and can be deployed from any available pot that is available
+//service names are used
 if(process.env.MONGO_DB_URI){
 	DB_URI = process.env.MONGO_DB_URI
 }
