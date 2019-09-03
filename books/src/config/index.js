eda95079fbc2410fe75060d5dfa013aa4140771e
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-09-03 17:17:09 +0100

    Commented on securing config  params.
    This includes authentication params with JWT.

--- books/src/config/index.js
+++ books/src/config/index.js
@@ -3,6 +3,7 @@ let DB_URI = "mongodb://localhost:27017/microservices-nodejs-docker-nginx";
 //Will be re-configured with nginx with service name for proxy-ing
 //The containers IP addresses are dynamic, and can be deployed from any available pot that is available
 //service names are used
+//Keep the DB_URI values in environmental variables and must not be published to your source control.
 if(process.env.MONGO_DB_URI){
 	DB_URI = process.env.MONGO_DB_URI
 }
