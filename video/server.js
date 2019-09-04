3afe2d6e3dc23d0dfc38f439dbc18f3b90ca16af
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-09-04 09:24:25 +0100

    Modified port number from disk read.

--- video/server.js
+++ video/server.js
@@ -6,6 +6,6 @@ const mongoose = require("mongoose");
 mongoose.connect(DB_URI);
 
 app.listen(3000, () => {
-	console.log("Running on port 8000");
+	console.log("Running on port 8050");
 	console.log(".....................")
 });
